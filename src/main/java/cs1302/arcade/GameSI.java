194212e3ba14a3e0ee85e62279f3a1d29ff29a0a
DANIEL MACDONALD <dam@nike.cs.uga.edu>
2019-05-03 20:57:54 -0400

    Added some basic visuals for space invaders

--- src/main/java/cs1302/arcade/GameSI.java
+++ src/main/java/cs1302/arcade/GameSI.java
@@ -3,19 +3,31 @@ import javafx.scene.Group;
 import javafx.scene.image.ImageView;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.VBox;
+import javafx.geometry.Pos;
+import javafx.scene.layout.StackPane;
 public class GameSI extends Group {
 
     ArcadeApp application;
     ImageView ship;
     ArcButton menu;
+    StackPane game;
+    VBox vbox;
+    
 
     public GameSI(ArcadeApp application) {
         this.application = application;
         ship = new ImageView(new Image("spaceInv/spaceship.png"));
+        game = new StackPane();
+        vbox = new VBox();
         
+        vbox.getChildren().add(game);
+        this.getChildren().addAll(vbox);
+        game.setAlignment(Pos.CENTER);
+        game.getChildren().addAll(new ImageView(new Image("spaceInv/space.png")),ship);
         menu = new ArcButton(0,0,new Image("2048/MainMenu.png"), e -> 
                              application.setScene(application.getScene()));
-        this.getChildren().addAll(ship,menu);
+        this.getChildren().addAll(menu);
     }
     
     
