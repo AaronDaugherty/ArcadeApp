3f458db59db4574ffe515eafc28db23d7f41de04
Aaron Daugherty <acd@uga.edu>
2019-05-07 04:03:38 -0400

    Added gameover.png

--- src/main/java/cs1302/arcade/Barrier.java
+++ src/main/java/cs1302/arcade/Barrier.java
@@ -4,17 +4,27 @@ import javafx.scene.shape.Rectangle;
 import javafx.scene.image.Image;
 import javafx.scene.paint.ImagePattern;
 
+/**
+ * Represents a barrier block object in SpaceInvaders.
+ *
+ */
+
 public class Barrier extends Rectangle {
 
-    private final ImagePattern barrier1 = new ImagePattern(new Image("spaceInv/barrier1.png"));
-    private final ImagePattern barrier2 = new ImagePattern(new Image("spaceInv/barrier2.png"));
-    private final ImagePattern barrier3 = new ImagePattern(new Image("spaceInv/barrier3.png"));
-    private final ImagePattern barrier4 = new ImagePattern(new Image("spaceInv/barrier4.png"));
+    private final ImagePattern BARRIER_1 = new ImagePattern(new Image("spaceInv/barrier1.png"));
+    private final ImagePattern BARRIER_2 = new ImagePattern(new Image("spaceInv/barrier2.png"));
+    private final ImagePattern BARRIER_3 = new ImagePattern(new Image("spaceInv/barrier3.png"));
+    private final ImagePattern BARRIER_4 = new ImagePattern(new Image("spaceInv/barrier4.png"));
 
     private int dmgLvl;
     private int width;
     private int height;
 
+    /**
+     *Constructor for Barrier class. Creates a barrier
+     *with a given length and width
+     *@param int length and width
+     */
     public Barrier(int length, int width){
         super(length, width);
 	this.setFill(barrier1);
@@ -23,9 +33,13 @@ public class Barrier extends Rectangle {
 	this.setDmgLvl(1);
     }
 
+    /**
+     *Constructor for Barrier class. 
+     *
+     */
     public Barrier() {
         super(30, 30);
-	this.setFill(barrier1);
+	this.setFill(BARRIER_1);
         this.width = 30;
         this.height = 30;
 	this.setDmgLvl(1);
@@ -34,16 +48,16 @@ public class Barrier extends Rectangle {
     public void setDmgLvl(int dmg) {
         this.dmgLvl = dmg;
         if(dmg == 1) {
-            this.setFill(barrier1); 
+            this.setFill(BARRIER_1); 
 	}
         if(dmg == 2) {
-            this.setFill(barrier2);
+            this.setFill(BARRIER_2);
         }
         if(dmg == 3) {
-            this.setFill(barrier3);
+            this.setFill(BARRIER_3);
         }
         if(dmg == 4) {
-            this.setFill(barrier4);
+            this.setFill(BARRIER_4);
         }
         if(dmg == 5) {
             this.setTranslateX(5000);
